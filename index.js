e9557ca97f7e7e11c90bcc3890394872f8bcb0b2
Amine Abisourour <amine.abisourour@expertizian.com>
2017-07-27 01:46:46 +0100

    time parse

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var bitcoin = require('bitcoinjs-lib');
 var beeper = require('beeper');
 var prefix = process.argv[2] || '';
 var prfxLen = prefix.length;
-
 var base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 var error;
-
-console.time('processTime');
 
 if (prefix && prfxLen) {
     if (prefix[0] !== '1') {
@@ -14,7 +11,7 @@ if (prefix && prfxLen) {
     } else if (prfxLen > 34) {
         error = 'Prefix must be less than or equal 34 characters';
     } else {
-        for (var i = 0; i < prfxLen; i++) {
+        for (i = 0; i < prfxLen; i++) {
             var c = prefix[i];
             if (!~base58.indexOf(c)) {
                 error = 'Prefix contains an invalid base58 character: ' + c;
@@ -28,10 +25,22 @@ if (prefix && prfxLen) {
     }
 }
 
-console.log('Searching for an address that start with: ' + prefix + '...\n');
-beeper();
+function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+    , seconds = parseInt((duration/1000)%60)
+    , minutes = parseInt((duration/(1000*60))%60)
+    , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds + "s";
+}
 
 function btc_van_gen(prefix) {
+    beeper();
+    console.log('Searching for an address that start with: ' + prefix + '...\n');
     var start = Date.now();
     var tryN = 0;
     var hit = false;
@@ -45,17 +54,15 @@ function btc_van_gen(prefix) {
         // Show every generated key pairs
         // console.log(tryN + " " + address + " " + secret);
         if (target === prefix) {
-            var end = Date.now();
             hit = true;
+            var end = Date.now();
             if (tryN === 1) {
                 console.log('Got one from the first try in %dms', (end - start));
             } else {
-                console.log('Got one after %d tries in %ds', tryN, (end - start)/1000);
+                console.log('Got one after ' + tryN + ' tries in ' + msToTime(end - start));
             }
             console.log('Public Key (address): ' + address);
             console.log('Private Key (secret): ' + secret);
-            // console.log("Time taken: %ds", (end - start)/1000);
-            // console.log(console.timeEnd('processTime'));
             beeper(3);
         }
     }
